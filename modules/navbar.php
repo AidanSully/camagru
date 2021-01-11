1105626225a1a0e2f78be5a5c8fd0ab91e601266
unknown <aidansully@github.com>
2021-01-11 16:51:26 +0100

    navbar pretty much done

---
+++ modules/navbar.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+<head>
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+<html>
+<nav>
+    <div class="logo">
+        <h4>Camagru</h4>
+    </div>
+    <ul class="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Logout</a></li>
+    </ul>
+    <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+</nav>
+<script src="../js/navbar.js"></script>
+
+</html>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,15 @@
-<?php declare(strict_types=1);
+<html lang="en">
 
-require __DIR__ . '/../src/test.php';
-
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
+    
+    <title>NAVIGATION</title>
+</head>
+<body>
+    <?php include_once('../modules/navbar.php'); ?>
+</body>
+</html>
